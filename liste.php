576290ee2984b7339b1d42745b33a4474ba99359
Soeren J <soerenj-os@web.e>
2017-09-10 18:16:03 +0200

    bugfix JS Ankersprung

--- liste.php
+++ liste.php
@@ -673,7 +673,7 @@ echo "
 //document.getElementById('div-sender-select').style.display='block';document.body.insertBefore(document.getElementById('div-sender-select'),document.getElementById('schnellauswahl'));
 //doen not work (probleme bei HBBTV): formItemFocus(document.getElementById('sender_alle_link') )
 
-echo "<a href='#sender_select' onClick=\"document.getElementById('div-sender-select').style.display='block';formItemFocus( document.getElementById('senderliste_2') );\"  id=\"link_sender_select\" class=\"link_black_before_onload\" tabindex=\"1\"><span style=\"background:yellow\"  class=\"hbbtv_button\">&nbsp;&nbsp;&nbsp;</span> Sender wählen&nbsp;&nbsp;&nbsp;";
+echo "<a href='#sender_select' onClick=\"document.getElementById('div-sender-select').style.display='block';formItemFocus( document.getElementById('senderliste_2') );window.setTimeout(function(){ formItemFocus( document.getElementById('senderliste_2') ); },50);\"  id=\"link_sender_select\" class=\"link_black_before_onload\" tabindex=\"1\"><span style=\"background:yellow\"  class=\"hbbtv_button\">&nbsp;&nbsp;&nbsp;</span> Sender wählen&nbsp;&nbsp;&nbsp;";
 if( isset($_GET['sender']))$s=$_GET['sender'];else $s='';
 echo "<span style=\"color:black\">$s <span id=\"sender_waehlen_append\"></span> </span>";
 echo "</a>";
