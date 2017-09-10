e1fcd84a735d42c70137d46a21d1d0a98c70b280
Soeren J <soerenj-os@web.e>
2017-09-10 15:54:50 +0200

    formatierung

--- inc/inc.senderliste_themenliste.php
+++ inc/inc.senderliste_themenliste.php
@@ -121,81 +121,81 @@ function getThemenliste($options){
         $ll = 0;
 
         foreach($senderThema[ strtolower($_GET['sender']) ] as $s=>$more_data ){
-          $ll++;
+              $ll++;
               $count    = $more_data['count'];
               $lastDate = $more_data['lastDate'];         
               if(isset($_GET['sender']) && $_GET['sender']=='alle') $senderUrlPart = 'sender='.$_GET['sender'].'&';
               if( isset($_GET['hide_shorter_then']) && $_GET["hide_shorter_then"]!='' )  $s3 = "&hide_shorter_then=".$_GET['hide_shorter_then']; else $s3="";
               $href     = "liste.php?".$senderUrlPart."thema=".rawurlencode(str_replace('\\"','"',$s))."".$s3;
-                if( isset($_GET["quality"]) && $_GET["quality"]!='' ) $s2 ="&quality=".$_GET["quality"]; else $s2 = '';
-                //if( isset($hideHoerfassung) && $hideHoerfassung!='1' ) $s2 .="&hideHoerfassung=".$hideHoerfassung;
+              if( isset($_GET["quality"]) && $_GET["quality"]!='' ) $s2 ="&quality=".$_GET["quality"]; else $s2 = '';
+              //if( isset($hideHoerfassung) && $hideHoerfassung!='1' ) $s2 .="&hideHoerfassung=".$hideHoerfassung;
               $b = strtoupper(substr($s,0,1));
               $lastBuchstabe = $b;
               if(!isset($allBuchstaben[$b])) $allBuchstaben[$b] = 0;
               $allBuchstaben[$b] += $count;
-                    if( !isset($allBuchstabenFirstEntry[$b]) ) $allBuchstabenFirstEntry[$b] = $ll;
+              if( !isset($allBuchstabenFirstEntry[$b]) ) $allBuchstabenFirstEntry[$b] = $ll;
 
               $themenListOutArray[$b][substr(str_replace('\\"','"',$s),0,65)] = $href.$s2;
 
-                $title = substr($s,0,50);
+              $title = substr($s,0,50);
               if($count>1)$title.= " ($count)";
               if( isset($more_data['aLen']) && $more_data['aLen']!==NULL ){
                   $title.= ' &nbsp; '.round($more_data['aLen']).'Min'; //durchschnittslaenge
                   if($count>1) $title.= '∅';
               }
               if( isset($more_data['sender']) )$title.= " ".implode(', ',$more_data['sender'])."";
-                if($lastDate!='') $lastDateFormat = de_getWochentag($lastDate+(60*60)).gmdate(', d.m.Y H:i', $lastDate+(60*60));
+              if($lastDate!='') $lastDateFormat = de_getWochentag($lastDate+(60*60)).gmdate(', d.m.Y H:i', $lastDate+(60*60));
               //if($lastDate!='') $title.= "<span class=\"t_sel_date\" >".$lastDateFormat."</span>";
-                    if($lastDate!='') $date = $lastDateFormat; else $date=''; //"<span class=\"t_sel_date\" >"....</span>
+              if($lastDate!='') $date = $lastDateFormat; else $date=''; //"<span class=\"t_sel_date\" >"....</span>
 
               $arrayThemen[ $b ][ $href ] = array('name'=>substr($s,0,50), 'count'=>"$count", 'title'=>$title, 'date'=>$date, 'lastDate'=>$lastDateFormat);
-              }
+          }
 
-              if( isset($allBuchstaben) ){
-                  $locale=(defined('PHP_OS') && stristr(PHP_OS, 'win')) ? 'German_Germany.65001' : 'de_DE.utf8';
-                  $oldLocale=setlocale(LC_COLLATE, "0");
-                  uksort($allBuchstaben, 'traceStrColl');
-                  setlocale(LC_COLLATE, $oldLocale);
-                  $dp = '';
-                  //$dp = 'display:none;';
-                  //if( $anker=='#thema_select' || strstr($anker,'#buchstabe_')!==false || strstr($anker,'#thema_sel_')!==false) $dp = 'display:inline-block';
-                  if( isset($_GET['sender']) && $_GET['sender']!='' && (!isset($_GET['thema']) || $_GET['thema']=='') ) $dp = 'display:inline-block';
-                  $out.= "<div style=\"$dp;width:100%;padding-left:1pt;text-indent: -10pt;padding-left: 11pt;margin-top: -1pt;\" id=\"thema_sel_buchstaben\"><span style=\"float:left\"><span style=\"background:blue\" class=\"hbbtv_button\">&nbsp;&nbsp;&nbsp;</span><!-- Springe zu -->Buchstabe:&nbsp; </span><div id=\"thema_sel_buchstaben_sub\">";
-                  $out.= "<script language=\"javascript\" type=\"text/javascript\"> possibleHideHbbTVButtons(); </script>";
-                  $possible_url_base = '';
+          if( isset($allBuchstaben) ){
+              $locale=(defined('PHP_OS') && stristr(PHP_OS, 'win')) ? 'German_Germany.65001' : 'de_DE.utf8';
+              $oldLocale=setlocale(LC_COLLATE, "0");
+              uksort($allBuchstaben, 'traceStrColl');
+              setlocale(LC_COLLATE, $oldLocale);
+              $dp = '';
+              //$dp = 'display:none;';
+              //if( $anker=='#thema_select' || strstr($anker,'#buchstabe_')!==false || strstr($anker,'#thema_sel_')!==false) $dp = 'display:inline-block';
+              if( isset($_GET['sender']) && $_GET['sender']!='' && (!isset($_GET['thema']) || $_GET['thema']=='') ) $dp = 'display:inline-block';
+              $out.= "<div style=\"$dp;width:100%;padding-left:1pt;text-indent: -10pt;padding-left: 11pt;margin-top: -1pt;\" id=\"thema_sel_buchstaben\"><span style=\"float:left\"><span style=\"background:blue\" class=\"hbbtv_button\">&nbsp;&nbsp;&nbsp;</span><!-- Springe zu -->Buchstabe:&nbsp; </span><div id=\"thema_sel_buchstaben_sub\">";
+              $out.= "<script language=\"javascript\" type=\"text/javascript\"> possibleHideHbbTVButtons(); </script>";
+              $possible_url_base = '';
+              if(isset($_GET['start']) && $_GET['start']!='' && isset($_GET['ende']) && $_GET['ende']!=''){
+                $p     = parse_url($_SERVER['REQUEST_URI']);
+                $anker = (isset($p["fragment"]))?$p["fragment"]:'';
+                $possible_url_base = $_SERVER['REQUEST_URI'];
+                $possible_url_base = preg_replace('/&start=-?([0-9]*)/','',$possible_url_base);
+                $possible_url_base = preg_replace('/&ende=-?([0-9]*)/','',$possible_url_base);
+                $possible_url_base = preg_replace('/&&/','&',$possible_url_base);
+                if($anker!='')$possible_url_base = preg_replace('/'.$anker.'/','',$possible_url_base);
+              }
+              foreach($allBuchstaben as $b=>$count){
                   if(isset($_GET['start']) && $_GET['start']!='' && isset($_GET['ende']) && $_GET['ende']!=''){
-                    $p     = parse_url($_SERVER['REQUEST_URI']);
-                    $anker = (isset($p["fragment"]))?$p["fragment"]:'';
-                    $possible_url_base = $_SERVER['REQUEST_URI'];
-                    $possible_url_base = preg_replace('/&start=-?([0-9]*)/','',$possible_url_base);
-                    $possible_url_base = preg_replace('/&ende=-?([0-9]*)/','',$possible_url_base);
-                    $possible_url_base = preg_replace('/&&/','&',$possible_url_base);
-                    if($anker!='')$possible_url_base = preg_replace('/'.$anker.'/','',$possible_url_base);
-                  }
-                  foreach($allBuchstaben as $b=>$count){
-                      if(isset($_GET['start']) && $_GET['start']!='' && isset($_GET['ende']) && $_GET['ende']!=''){
-                        $diff  = $_GET['ende'] - $_GET['start'];
-                        $s = floor($allBuchstabenFirstEntry[$b]/$diff)*$diff;
-                        $possible_url= $possible_url_base.'&start='.$s.'&ende='.($s+$diff).'';
-                      } else $possible_url = '';
-                   $out.= "&nbsp;<a href=\"$possible_url#buchstabe_".rawurlencode(utf8_encode($b))."\" data-starts-with-no=\"$ll\" onClick=\"document.getElementById('list_auswahl_links_thema').style.display='block';\" class=\"buchstaben_anker_link\" title=\"$count Filme\"><b>".utf8_encode($b)."</b>&nbsp;<small class=\"b_count\">($count)</small> </a> &nbsp;\n";  
-                  }
-                  $out.= "
-                   <script language=\"javascript\" type=\"text/javascript\">
-                       if(screen.availWidth>800 && window.innerHeight>300 && screen.availHeight>500)document.getElementById('start').appendChild(  document.getElementById('thema_sel_buchstaben') );
-                       else{ // wenn Bildschirm zu klein: Buchstabenlinks nicht in fester Headerleiste, sondern oberhalb der Themen (scrollbar)
-                          var r = document.getElementById('list_auswahl_links_thema');
-                          r.insertBefore(  document.getElementById('thema_sel_buchstaben'), r.firstChild );
-                         }
-                      //function onload2y(){   } //window.addEventListener(\"load\", onload2y); 
-                   </script>";
-                  $out.= "</div></div><br><br>\n";
+                    $diff  = $_GET['ende'] - $_GET['start'];
+                    $s = floor($allBuchstabenFirstEntry[$b]/$diff)*$diff;
+                    $possible_url= $possible_url_base.'&start='.$s.'&ende='.($s+$diff).'';
+                  } else $possible_url = '';
+                  $out.= "&nbsp;<a href=\"$possible_url#buchstabe_".rawurlencode(utf8_encode($b))."\" data-starts-with-no=\"$ll\" onClick=\"document.getElementById('list_auswahl_links_thema').style.display='block';\" class=\"buchstaben_anker_link\" title=\"$count Filme\"><b>".utf8_encode($b)."</b>&nbsp;<small class=\"b_count\">($count)</small> </a> &nbsp;\n";  
               }
+              $out.= "
+               <script language=\"javascript\" type=\"text/javascript\">
+                   if(screen.availWidth>800 && window.innerHeight>300 && screen.availHeight>500)document.getElementById('start').appendChild(  document.getElementById('thema_sel_buchstaben') );
+                   else{ // wenn Bildschirm zu klein: Buchstabenlinks nicht in fester Headerleiste, sondern oberhalb der Themen (scrollbar)
+                      var r = document.getElementById('list_auswahl_links_thema');
+                      r.insertBefore(  document.getElementById('thema_sel_buchstaben'), r.firstChild );
+                     }
+                  //function onload2y(){   } //window.addEventListener(\"load\", onload2y); 
+               </script>";
+              $out.= "</div></div><br><br>\n";
+          }
 
-              $out.= $listThemenOut;
-              $out.= "</div>";
-              //if(isset($_GET['sender']) && $_GET['sender']!='' && (!isset($_GET['thema']) && $_GET['thema']==''))$out.= "<script language=\"javascript\"  type=\"text/javascript\">var liste_from_select_thema = JSON.parse('".json_encode($arrayThemen)."');</script>";
-              //file_put_contents($file.'__cache__select_thema',$out);
+          $out.= $listThemenOut;
+          $out.= "</div>";
+          //if(isset($_GET['sender']) && $_GET['sender']!='' && (!isset($_GET['thema']) && $_GET['thema']==''))$out.= "<script language=\"javascript\"  type=\"text/javascript\">var liste_from_select_thema = JSON.parse('".json_encode($arrayThemen)."');</script>";
+          //file_put_contents($file.'__cache__select_thema',$out);
        }
       //}else{
       //   $out = file_get_contents($file.'__cache__select_thema');  
