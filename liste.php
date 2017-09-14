ee2ea5b979dce5081a396a885a4d1586b17dcebe
Soeren J <soerenj-os@web.e>
2017-09-14 22:45:38 +0200

    bugfix unset var JS

--- liste.php
+++ liste.php
@@ -150,8 +150,8 @@ function onload1y(){ //onload
     var countFavs = 0;       var raw = getCookie('favs');     if(raw=='')raw='{}'; var countFavs = JSON.parse(raw).length;
     if( countFavs> 2 && document.getElementById('schnellauswahl_list__jump_to_filme')!=undefined && document.getElementsByClassName('videolink_row').length>0 ) document.getElementById('schnellauswahl_list__jump_to_filme').style = 'inline-block';
     
-    if(getCookie('extra_sender_AudioDeskription')!=1) document.getElementsByClassName('sender__alle_ad')[0].style.display = 'none';
-    if(getCookie('extra_sender_Gebaerdensprache')!=1) document.getElementsByClassName('sender__alle_gebaerde')[0].style.display = 'none';
+    if(getCookie('extra_sender_AudioDeskription')!=1 && document.getElementsByClassName('sender__alle_ad').length>0) document.getElementsByClassName('sender__alle_ad')[0].style.display = 'none';
+    if(getCookie('extra_sender_Gebaerdensprache')!=1 && document.getElementsByClassName('sender__alle_gebaerde').length>0) document.getElementsByClassName('sender__alle_gebaerde')[0].style.display = 'none';
     
     var c = getCookie('showFooter'); if(c>0) document.getElementById('fixed_footer').style.display = 'block';
     
