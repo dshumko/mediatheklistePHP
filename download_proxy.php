9fdc28f04362c8e8ca3ef08d38cec8bad7d043d3
soerenj <soerenj-os@web.de>
2018-03-26 20:06:09 +0200

    Update download_proxy.php

--- download_proxy.php
+++ download_proxy.php
@@ -3,42 +3,54 @@
 //Diese Datei ist nur optional
 
 /*
-Eine Weiterlöeitung (Proxy) zum DOWNLOAD (nur Download, nicht direkt anschauen) vom Filmdateien.
+Eine Weiterleitung (Proxy) zum Download/ansehen vom Filmdateien.
 Das ist bspw. sinnvoll wenn man keine Deutsche IP-Adresse hat.
 
-Download Proxy Ist eine mögliche SICHERHEITS-LÜCKE
-Die Überprüfung der Adresse ist sehr einfach ausgelegt.
-Daher sollte diese Funktion nur aktiviert werden bei Private+Geheimen installationen.
+Dieser Download Proxy ist eine mögliche SICHERHEITS-LÜCKE.
+Daher empfehle ich diese Funktion nur zu aktivieren bei ::::: Private+Geheimen ::::: Installationen.
+Auch sollte mal hiebei das Urheber-Recht beachten; Daher nochmal: Nur für Private und Geheime installation (keine öffentliche Aufführung; Man könnte die Installtion am besten auch Passwort schüztzen )
+Außerdem entsteht bei der Verwendung dieses Proxy's viel Daten-Traffic (Bitte eigenen WebServer-Tarif beachten um kosten zu vermeiden)
+Die Sicherheits-Überprüfung der Adresse ist sehr !!!!!!unsicher!!!!! Es besteht die Gefahr das Hacker diese ausnutzen um dei Seiten der öffentlich Rechlichen zu attaktieren. 
+
 Aktivieren:
 1) Diese Zeile löschen:
    die('deaktiviert');
 2) Config.inc.php bearbeiten:
    $dereff = 'download_proxy.php?url='; 
-3) Config.inc.php
-   $fullscreen_play darf nicht auf =1 stehen ! (am besten $fullscreen_play=0;)
+3) Config.inc.php bearbeiten:
+   $fullscreen_play = 0;
 */
 
-die('deaktiviert');
+die('deaktiviert! ACHTUNG: Verwendung des Proxys unsicher!!!! Nur bei Privaten und Passwort-geschützen installationen (mögliche Gefahr: das Hacker dieses verwenden um die Seiten der Medienanstalten anzugreifen)');
 
 if( !isset($_GET['url']) ) die("keine ?url=...");
 if( count($_GET['url'])>512 ) die("url zu lang");
-$url_end = end( explode('/',$_GET['url']) );
 
-$url_cuted = str_replace($url_end, '', $_GET['url']);
+$_GET['url'] = str_replace('http://','', $_GET['url']); $_GET['url'] = 'http://'.$_GET['url']; //muss http:// drin haben
 
-if( !stristr(file_get_contents('Filmliste-akt'), $url_cuted) ) die("url (".strip_tags($url_cuted).")  nicht gefunden in Filmliste.<br>Mögliche Gründe für den Fehler:<br>-Fehler im Programm (versuche einen anderen Sender)<br>-Film ist inwzischen offline<br>-du bist ein Hacker");
+$url_cuted = str_replace( basename($_GET['url']) , '', $_GET['url']); //lösche in URL: filmdatei.mp4 (o.ä.)
 
+
+//Prüfe ob URL in Filmliste vorhanden ist
+if( !stristr(file_get_contents('Filmliste-akt'), $url_cuted) ){
+	//prüfe nochmal auf https:// URL
+	if( !stristr(file_get_contents('Filmliste-akt'), str_replace('http://','https://',$url_cuted)) )
+	{
+		die("url (".strip_tags($url_cuted).")  nicht gefunden in Filmliste.<br>Mögliche Gründe für den Fehler:<br>-Fehler im Programm (versuche einen anderen Sender)<br>-Film ist inwzischen offline<br>-du bist ein Hacker");
+	}
+}
 $url_secure = explode('?',$_GET['url'])[0];
-
-
-$url = $_GET['url'];
+$url = $url_secure
 
 
 exec("curl -s -X HEAD -I --connect-timeout 10 -m 10 ".$url." 2>&1  | grep '^Content-Length: '",$return);
 $filesize = str_replace('Content-Length: ','',$return[0]);
-                                 
+if($filesize==0) die("Datei nicht gefunden");
 
-header("Content-Disposition: attachment; filename=\"".basename($_GET['url'])."\";" );
+//if($filesize<10485760) die("Da Stimmt was nicht.<br>Die Datei sehr klein (<10MB); Vermutlich ist es daher kein Video und der Zugriff über Proxy wurde gesperrt.<br>Direkter Zugriff ohne Proxy über diese Adresse:<br>".strip_tags($_GET[''])); //Das funktioniert nicht als Sicherheits-maßname. Grund: da nach einen HTTP HEAD-Anfrage wahrscheinlich schon die Anfrage/bzw. Hackangriff ausgeführt wurden (?)
+                                 
+//header("Content-Disposition: attachment; filename=\"".basename($_GET['url'])."\";" ); //Datei direkt runterladen
+if( substring($url,-4)=='.mp4')header("Content-Type: video/mp4" );
 header("Content-Transfer-Encoding: binary");
 header("Content-Length: ".$filesize);
 
